c35cc7592ef27428910fcf04a1a63210296554ba
jjeonjjunyyeong <wjswnsdud354@gmail.com>
2023-11-27 22:43:53 +0900

    add gitignore

--- local_server_test/routes/2.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    console.log("Second Page");
-    return res.sendFile('C:\\Users\\wnsdu\\Desktop\\test\\public\\2.html');
-});
-
-module.exports = router;
--- local_server_test/routes/3.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    console.log("Third Page");
-    return res.sendFile('C:\\Users\\wnsdu\\Desktop\\test\\public\\3.html');
-});
-
-module.exports = router;
