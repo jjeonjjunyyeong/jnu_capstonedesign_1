68006b5ea57fecb8fd15eeb80fba37de7d607f99
jjeonjjunyyeong <wjswnsdud354@gmail.com>
2023-12-03 18:26:34 +0900

    add to new algorithm

--- 12.02/11월29일22시/app.py
+++ 12.02/11월29일22시/app.py
@@ -8,8 +8,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 
-user_information = dict()
+result = dict()
 
+places_information = ""
 app = Flask(__name__)
 
 
@@ -23,7 +24,8 @@ def test():
     if request.method == "POST":
         global result
         result = request.form
-        position = airplane(result)  # 지역 명소
+
+        position = airplane(result)
         # d(result) # 날짜 계산
         return render_template("airplane.html", result=position)
     else:
@@ -33,17 +35,18 @@ def test():
 @app.route("/a", methods=["GET", "POST"])
 def test1():
     if request.method == "POST":
-        position = places(result["place"])  # 지역 명소
-        
+        position = places(result)  # 지역 명소
+        print(type(position))
         return render_template("places.html", result=position)
 
 
 @app.route("/b", methods=["GET", "POST"])
 def test2():
     if request.method == "POST":
-        position = hotel(result)  # 지역 명소
-
-        return render_template("places.html", result=position, to=result)
+        places_information = request.form.getlist("hi")
+        position = hotel(result)  # 슥소
+        print(places_information)
+        return render_template("hotel.html", result=position)
 
 
 if __name__ == "__main__":
